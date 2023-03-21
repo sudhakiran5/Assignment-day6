a0386c97319c3955ec94eec86a76905fa4410b65
sudhakiran5 <127855895+sudhakiran5@users.noreply.github.com>
2023-03-21 20:44:43 +0530

    Update flipcoin.sh

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-n=$1
-result=1
-echo "The table of powers of 2 is: "
-while (( result < 256 ))
+heads_count=0
+tails_count=0
+while (( heads_count != 11 && tails_count != 11))
 do
-    result=$(( result * 2 ))
-    echo "$result"
+    echo -n "......flipping.....it's"
+    toss=$(( RANDOM % 2 ))
+    if [ $toss -eq 0 ]
+    then
+    echo " Heads "
+    (( heads_count ++ ))
+    else
+    echo " Tails "
+    (( tails_count ++ ))
+    fi
+    echo "Head wins $heads_count times and Tail wins $tails_count times"
 done
+echo "Game Over!"
